a8879e8150242c886ebd974fc7108cb45efa76a4
Laurent Lao <lao.laurent@gmail.com>
2021-07-13 13:05:11 -0400

    Frontend works remote and local

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://frozen-fortress-76550.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
