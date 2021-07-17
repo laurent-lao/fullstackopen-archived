39901d53098ba24d19d68b941add473bdcf9d92c
Laurent Lao <lao.laurent@gmail.com>
2021-07-17 17:19:13 -0400

    Blogs have id property instead of _id

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -6,42 +6,50 @@ const helper = require('./test_helper')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
+describe('api tests', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
 
-  // Parallel
-  const blogObjects = helper.initialBlogs
-    .map((blog) => new Blog(blog))
-  const promiseArray = blogObjects
-    .map((note) => note.save())
-  await Promise.all(promiseArray)
+    // Parallel
+    const blogObjects = helper.initialBlogs
+      .map((blog) => new Blog(blog))
+    const promiseArray = blogObjects
+      .map((note) => note.save())
+    await Promise.all(promiseArray)
 
-  // One By One
-  // let blogObject = new Blog(helper.initialBlogs[0])
-  // await blogObject.save()
-  // blogObject = new Blog(helper.initialBlogs[1])
-  // await blogObject.save()
+    // One By One
+    // let blogObject = new Blog(helper.initialBlogs[0])
+    // await blogObject.save()
+    // blogObject = new Blog(helper.initialBlogs[1])
+    // await blogObject.save()
 
-  // Sequentially FORCE ORDER (forEach does not work well in async)
-  // for (let blog of helper.initialBlogs) {
-  //   let blogObject = new Blog(blog)
-  //   await blogObject.save()
-  // }
-})
+    // Sequentially FORCE ORDER (forEach does not work well in async)
+    // for (let blog of helper.initialBlogs) {
+    //   let blogObject = new Blog(blog)
+    //   await blogObject.save()
+    // }
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('there are six blogs', async () => {
-  const response = await api.get('/api/blogs')
+  test('there are six blogs', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(6)
-})
+    expect(response.body).toHaveLength(6)
+  })
 
-afterAll(() => {
-  mongoose.connection.close()
+  test('blogs have property \'id\'', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.map((item) => expect(item.id).toBeDefined())
+  })
+
+  afterAll(() => {
+    mongoose.connection.close()
+  })
 })
