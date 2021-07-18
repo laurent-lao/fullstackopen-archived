766dce63890ec994402eaa3635b101f351cafe82
Laurent Lao <lao.laurent@gmail.com>
2021-07-18 01:38:52 -0400

    Add users controller

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,8 +4,9 @@ require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
